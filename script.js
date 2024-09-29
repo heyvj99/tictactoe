71212d329a0bb00d0edbececd2a1c295d18e32ae
heyvj <vijayalaxmi9915@gmail.com>
2024-09-28 22:20:21 -0700

    the basic working version ready

---
+++ script.js
@@ -0,0 +1,180 @@
+function CreatePlayer(symbol) {
+  const playerName = symbol;
+  let getPlayerName = () => playerName;
+
+  return { getPlayerName };
+}
+
+function Gameboard(player1, player2) {
+  let allTiles = new Array(9).fill("");
+  let currPlayer = player2;
+  let isGameOver = false;
+
+  let updateTile = (event, tileId) => {
+    allTiles[Number(tileId)] = currPlayer.getPlayerName();
+    event.target.innerHTML = currPlayer.getPlayerName();
+  };
+
+  let getAllTiles = () => allTiles;
+
+  let toggleTurn = () => {
+    currPlayer = currPlayer == player1 ? player2 : player1;
+    document.querySelector(
+      ".whoseturn"
+    ).innerHTML = `Now Playing: ${currPlayer.getPlayerName()}`;
+  };
+
+  let getCurrentPlayer = () => currPlayer;
+
+  let isWinner = (tileId) => {
+    let mark = currPlayer.getPlayerName();
+    switch (Number(tileId)) {
+      case 0:
+        if (
+          (allTiles[0] == mark && allTiles[1] == mark && allTiles[2] == mark) ||
+          (allTiles[0] == mark && allTiles[3] == mark && allTiles[6] == mark) ||
+          (allTiles[0] == mark && allTiles[4] == mark && allTiles[8] == mark)
+        ) {
+          return true;
+        }
+        break;
+
+      case 1:
+        if (
+          (allTiles[1] == mark && allTiles[0] == mark && allTiles[2] == mark) ||
+          (allTiles[1] == mark && allTiles[4] == mark && allTiles[7] == mark)
+        ) {
+          return true;
+        }
+        break;
+
+      case 2:
+        if (
+          (allTiles[0] == mark && allTiles[1] == mark && allTiles[2] == mark) ||
+          (allTiles[2] == mark && allTiles[4] == mark && allTiles[6] == mark) ||
+          (allTiles[2] == mark && allTiles[5] == mark && allTiles[8] == mark)
+        ) {
+          return true;
+        }
+        break;
+
+      case 3:
+        if (
+          (allTiles[0] == mark && allTiles[3] == mark && allTiles[6] == mark) ||
+          (allTiles[3] == mark && allTiles[4] == mark && allTiles[5] == mark)
+        ) {
+          return true;
+        }
+        break;
+
+      case 4:
+        if (
+          (allTiles[2] == mark && allTiles[4] == mark && allTiles[6] == mark) ||
+          (allTiles[3] == mark && allTiles[4] == mark && allTiles[5] == mark) ||
+          (allTiles[0] == mark && allTiles[4] == mark && allTiles[8] == mark) ||
+          (allTiles[1] == mark && allTiles[4] == mark && allTiles[7] == mark)
+        ) {
+          return true;
+        }
+        break;
+
+      case 5:
+        if (
+          (allTiles[3] == mark && allTiles[4] == mark && allTiles[5] == mark) ||
+          (allTiles[2] == mark && allTiles[5] == mark && allTiles[8] == mark)
+        ) {
+          return true;
+        }
+        break;
+
+      case 6:
+        if (
+          (allTiles[1] == mark && allTiles[3] == mark && allTiles[6] == mark) ||
+          (allTiles[6] == mark && allTiles[7] == mark && allTiles[8] == mark) ||
+          (allTiles[6] == mark && allTiles[4] == mark && allTiles[2] == mark)
+        ) {
+          return true;
+        }
+        break;
+
+      case 7:
+        if (
+          (allTiles[1] == mark && allTiles[4] == mark && allTiles[7] == mark) ||
+          (allTiles[6] == mark && allTiles[7] == mark && allTiles[8] == mark)
+        ) {
+          return true;
+        }
+        break;
+
+      case 8:
+        if (
+          (allTiles[0] == mark && allTiles[4] == mark && allTiles[8] == mark) ||
+          (allTiles[6] == mark && allTiles[7] == mark && allTiles[8] == mark) ||
+          (allTiles[2] == mark && allTiles[5] == mark && allTiles[8] == mark)
+        ) {
+          return true;
+        }
+        break;
+
+      default:
+        return false;
+    }
+  };
+
+  let gameOver = (isWinner) => {
+    if (isWinner) {
+      setTimeout(() => {
+        console.log("Player Won");
+        document.querySelector(
+          ".gbcontainer"
+        ).innerHTML = `<span> ${currPlayer.getPlayerName()} Wins!</span>`;
+      }, 1000);
+    } else {
+      setTimeout(() => {
+        console.log("GAME OVER");
+        document.querySelector(
+          ".gbcontainer"
+        ).innerHTML = `<span> Game Over </span> `;
+      }, 1000);
+    }
+  };
+
+  return {
+    updateTile,
+    getAllTiles,
+    toggleTurn,
+    getCurrentPlayer,
+    isWinner,
+    gameOver,
+  };
+}
+
+let game = (() => {
+  let player1 = CreatePlayer("X");
+  let player2 = CreatePlayer("O");
+  let counter = 0;
+  // console.log(player1.getPlayerName());
+  // console.log(player2.getPlayerName());
+
+  let board = Gameboard(player1, player2);
+  board.toggleTurn();
+  console.log(board.getCurrentPlayer().getPlayerName());
+  board.toggleTurn();
+  console.log(board.getAllTiles());
+
+  let allTiles = document.querySelectorAll(".tile");
+  allTiles.forEach((aTile) => {
+    aTile.addEventListener("click", (evt) => {
+      board.updateTile(evt, evt.target.id);
+      let gameover = board.isWinner(evt.target.id);
+      counter++;
+      if (gameover) {
+        board.gameOver(true);
+      } else if (counter == 9) {
+        board.gameOver(false);
+      } else {
+        board.toggleTurn();
+      }
+    });
+  });
+})();
